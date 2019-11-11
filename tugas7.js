7adcb7209db41ca39fb6b231b1180cece78dc54c
Havid <great_havid@yahoo.co.id>
2019-11-11 14:21:44 +0700

    Tugas 7 JS

---
+++ tugas7.js
@@ -0,0 +1,13 @@
+// ====== Tugas 7 JavaScript =====
+
+var height = [111,122,133,144,155,166,177,123,134,135];
+
+console.log("-FOR-")
+for(var i=0; i<height.length; i++) {
+	console.log(height[i])
+}
+
+console.log("==Perulangan For OFF==")
+for(var a of height) {
+	console.log(a)
+}
